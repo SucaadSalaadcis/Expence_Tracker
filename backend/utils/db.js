2e269e703826dbfdee90873f26ee55a05fbc48ae
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2025-05-20 21:29:05 +0300

    added mongodb atlas

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config(); // make sure to load env before using variables
 
 const DbConnect = async () => {
     try {
-        await mongoose.connect(`${process.env.DB}`);
-        console.log("DB Connected Successfully...")
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@expencetracker.qzdwen7.mongodb.net/Expence_Tracker?retryWrites=true&w=majority&appName=expenceTracker`);
+        console.log("Database connected successfully...");
     } catch (error) {
-        console.log(error)
+        console.error("Error connecting to DB:", error.message);
     }
-}
+};
 
 export default DbConnect;
